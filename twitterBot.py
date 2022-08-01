9447a9706ed05714f9ee8493b611f2e3d9cd1f81
Omar Abdallah <23oabdalah@gmail.com>
2022-08-01 19:01:16 +0300

    Update twitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -35,28 +35,32 @@ def reply():
     tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode="extended")
     for tweet in reversed(tweets):
         if tweet.in_reply_to_status_id:
-            og_tweet = api.get_status(tweet.in_reply_to_status_id)
+            og_tweet = api.get_status(tweet.in_reply_to_status_id, tweet_mode="extended")
             if 'media' in og_tweet.entities:
+                print(og_tweet.entities)
                 for image in  og_tweet.entities['media']:
                     image_link = image['media_url'] + ":small"
                     get_image_from_url(image_link)
 
                 print("Tweet Detected!: " + str(tweet.id) + " - " + tweet.full_text.lower())
-                if (colorDetection.percent_grey > 0.63):
-                    tweet_message = ' The image attached to the original tweet is fabricated or has been manipulated with a certanity of ' + str(colorDetection.percentCertanity) + '% ❌'
-                elif (colorDetection.percent_grey <= 0.63):
-                    tweet_message = ' The image attached to the original tweet is real (minimal manipulation) with a certanity of ' + str(colorDetection.percentCertanity) + '% ✅'
+                if (colorDetection.percent_grey > 0.636):
+                    tweet_message = ' The image attached to the original tweet is fabricated or has been manipulated with a certanity of ' + str(colorDetection.percentCertanity) + '% ' + colorDetection.emoji + '\n\n' + '⚠️ This bot is still under development, please do not take any result as a concrete statement ⚠️'
+                elif (colorDetection.percent_grey <= 0.636):
+                    tweet_message = ' The image attached to the original tweet is authentic (minimal manipulation) with a certanity of ' + str(colorDetection.percentCertanity) + '% ' + colorDetection.emoji + '\n\n' + '⚠️ This bot is still under development, please do not take any result as a concrete statement ⚠️'
                 print(tweet.id)
                 api.update_status('@' + tweet.user.screen_name + tweet_message, tweet.id)
 
             elif 'media' not in og_tweet.entities:
-                print(tweet.entities)
+                print(og_tweet.entities)
                 print("No Image!: " + str(tweet.id) + " - " + tweet.full_text.lower())
-                api.update_status('@' + tweet.user.screen_name + "The tweet doesn't include an image to be analyzed", tweet.id)
+                ###api.update_status('@' + tweet.user.screen_name + " This tweet doesn't include an image that can be analyzed for manipulation ⚠️", tweet.id)
 
         api.create_favorite(tweet.id)
         store_last_seen(FILE_NAME, tweet.id)
 
 while True:
-    reply()
-    time.sleep(30)
+    try:
+        reply()
+        time.sleep(30)
+    except: 
+            continue
